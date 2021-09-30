8cea48007e9af344e190c37e85fde789ef13dc56
LevPewPew <levthedev@protonmail.com>
2021-09-30 20:28:13 +1000

    submitting data to the backend auth endpoint

--- src/components/RequestInviteForm.tsx
+++ src/components/RequestInviteForm.tsx
@@ -14,7 +14,7 @@ interface Props {
   onSubmit: () => void;
 }
 
-interface PostRequestBody {
+interface FormData {
   fullName: string;
   email: string;
   confirmEmail: string;
@@ -38,16 +38,38 @@ export const RequestInviteForm = ({ onSubmit }: Props) => {
   emailValue.current = watch("email", "");
 
   // NEXT fix any, probably an interface of the form data expected
-  const postFormData = (values: PostRequestBody) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        // @ts-ignore
-        resolve();
-      }, 2000);
-    });
+  const postFormData = async (values: FormData) => {
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     alert(JSON.stringify(values, null, 2));
+    //     // @ts-ignore
+    //     resolve();
+    //   }, 2000);
+    // });
+
+    const authEndpoint =
+      "https://us-central1-blinkapp-684c1.cloudfunctions.net/fakeAuth";
+
+    const authBody = {
+      name: values.fullName,
+      email: values.email,
+    };
+
+    try {
+      const response = await fetch(authEndpoint, {
+        method: "POST",
+        body: JSON.stringify(authBody),
+      });
+      const data = await response.json();
+      alert(JSON.stringify(data, null, 2));
+    } catch (dasErr) {
+      console.log({ dasErr });
+    }
+    // https://us-central1-blinkapp-684c1.cloudfunctions.net/fakeAuth
   };
 
+  // NEXT add 3 chars long minimum name format
+  // NEXT add email format validation
   return (
     <Box>
       <form onSubmit={handleSubmit(postFormData)}>
